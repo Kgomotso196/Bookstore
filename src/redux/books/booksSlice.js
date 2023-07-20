999edd02e1c1e8e476b5f149a0d1bb571bff67e2
Kgomotso Nacane <kgomotsonacane@gmail.com>
2023-07-20 12:32:25 +0200

    Add Redux slice for managing books state

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,7 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  books: [],
+  books: [
+    {
+      itemId: 'item1',
+      title: 'To Kill a Mockingbird',
+      author: 'Harper Lee',
+      category: 'Fiction, Classic',
+    },
+    {
+      itemId: 'item2',
+      title: '1984',
+      author: 'George Orwell',
+      category: 'Dystopian, Science Fiction',
+    },
+    {
+      itemId: 'item3',
+      title: 'Pride and Prejudice',
+      author: 'Jane Austen',
+      category: 'Fiction, Classic, Romance',
+    },
+  ],
 };
 
 export const BookSlice = createSlice({
@@ -12,7 +31,7 @@ export const BookSlice = createSlice({
       state.books.push(action.payload);
     },
     removeBook: (state, action) => {
-      state.books = state.books.filter((book) => book.id !== action.payload);
+      state.books = state.books.filter((book) => book.itemId !== action.payload);
     },
   },
 });
