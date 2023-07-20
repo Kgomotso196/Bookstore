de2f292c2770da0d852d456254dfe41afdf0e660
Kgomotso Nacane <kgomotsonacane@gmail.com>
2023-07-20 12:31:10 +0200

    Add empty line at end of source file

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-function Categories() {
-  return (
-    <div>Categories</div>
-  );
-}
+const Categories = () => {
+  const categoriesStatus = useSelector((state) => state.category.status);
+
+  const style = { marginLeft: '40px' };
+
+  return <h1 style={style}>{categoriesStatus}</h1>;
+};
 
 export default Categories;
